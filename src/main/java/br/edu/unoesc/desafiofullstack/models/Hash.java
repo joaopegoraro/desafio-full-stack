8415b0a20cc015faf4f08ec42562b190ed902160
João Pegoraro <hahmraro@tuta.io>
2023-11-10 19:11:23 -0300

    Adicionei entidade de Hash (senha do usuário)

--- src/main/java/br/edu/unoesc/desafiofullstack/models/Hash.java
+++ src/main/java/br/edu/unoesc/desafiofullstack/models/Hash.java
@@ -1,12 +1,16 @@
 package br.edu.unoesc.desafiofullstack.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class User {
+public class Hash {
     @Id
     private String username;
+
+    @Column(nullable = false)
+    private String hash;
 
     public String getUsername() {
         return username;
@@ -14,5 +18,13 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
     }
 }
