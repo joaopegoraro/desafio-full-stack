fe2c126b3d43fb2b2aadc641cc3fc3a021756d10
João Pegoraro <hahmraro@tuta.io>
2023-11-11 18:13:26 -0300

    Finalizei interface da tela de login

--- src/main/java/br/edu/unoesc/desafiofullstack/controllers/AuthController.java
+++ src/main/java/br/edu/unoesc/desafiofullstack/controllers/AuthController.java
@@ -2,7 +2,6 @@ package br.edu.unoesc.desafiofullstack.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
