9a6a08e87b75a3fa1252e66c381db23d9bd5250d
João Pegoraro <hahmraro@tuta.io>
2023-11-11 22:21:55 -0300

    Adicionei 'AuthService', serviço que lida com as rotinas de autenticação (login, cadastro)

---
+++ src/main/java/br/edu/unoesc/desafiofullstack/services/AuthService.java
@@ -0,0 +1,68 @@
+package br.edu.unoesc.desafiofullstack.services;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import org.springframework.stereotype.Service;
+
+import br.edu.unoesc.desafiofullstack.exceptions.BadCredentialsException;
+import br.edu.unoesc.desafiofullstack.exceptions.InternalException;
+import br.edu.unoesc.desafiofullstack.models.Hash;
+import br.edu.unoesc.desafiofullstack.models.User;
+import br.edu.unoesc.desafiofullstack.repositories.HashRepository;
+import br.edu.unoesc.desafiofullstack.repositories.UserRepository;
+import br.edu.unoesc.desafiofullstack.utils.PasswordHasher;
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final HashRepository hashRepository;
+
+    public AuthService(UserRepository userRepository, HashRepository hashRepository) {
+        this.userRepository = userRepository;
+        this.hashRepository = hashRepository;
+    }
+
+    public boolean isUserAuthenticated(HttpSession session) {
+        try {
+            return session.getAttribute("username") != null;
+        } catch (IllegalStateException e) {
+            return false;
+        }
+    }
+
+    public void authenticateSession(HttpSession session, String username) {
+        session.setAttribute("username", username);
+    }
+
+    public void performLogin(String username, String password) throws BadCredentialsException, InternalException {
+        final Hash hash = hashRepository.findById(username).orElseThrow(() -> new BadCredentialsException());
+        if (userRepository.findById(username).isEmpty()) {
+            hashRepository.deleteById(username);
+            throw new BadCredentialsException();
+        }
+
+        try {
+            final boolean isPasswordValid = !PasswordHasher.validatePassword(password, hash.getHash());
+            if (!isPasswordValid) {
+                throw new BadCredentialsException();
+            }
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new InternalException(e);
+        }
+    }
+
+    public void createAccount(User userDto) throws InternalException {
+        final User user = userRepository.save(userDto);
+        final Hash hash = new Hash();
+        hash.setUsername(user.getUsername());
+        try {
+            hash.setHash(PasswordHasher.hashPassword(user.getUsername()));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new InternalException(e);
+        }
+        hashRepository.save(hash);
+    }
+}
