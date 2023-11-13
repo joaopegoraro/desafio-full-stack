232a6bfc3178696b40b257ae0fc5d551e4da0dad
João Pegoraro <hahmraro@tuta.io>
2023-11-13 13:08:01 -0300

    Adicionei entidade Contact

---
+++ src/main/java/br/edu/unoesc/desafiofullstack/models/Contact.java
@@ -0,0 +1,54 @@
+package br.edu.unoesc.desafiofullstack.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_username")
+    private Person person;
+
+    private String phone;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
